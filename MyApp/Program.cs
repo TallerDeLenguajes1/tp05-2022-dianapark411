709af1f192746793904539805563d708be2db17a
Diana Park <dianapark411@gmail.com>
2022-05-17 16:23:04 -0300

    rama3

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -9,7 +9,7 @@ string numero1 = Console.ReadLine();
 Console.WriteLine("Ingrese otro numero:");
 string numero2 = Console.ReadLine();
 
-int resultado = Convert.ToInt32(numero1) + Convert.ToInt32(numero2);
+string resultado = numero1 + numero2;
 // int resultado = Int32.Parse(numero1) + Int32.Parse(numero2);
 
 string resultado2 = numero1 + numero2;
