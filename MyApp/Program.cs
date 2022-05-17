ad4cbd769ef917bc853d2c42fa5224011b821dfb
Diana Park <dianapark411@gmail.com>
2022-05-17 15:55:27 -0300

    cuarto commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,4 +3,15 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Ingrese texto:");
 string texto = Console.ReadLine();
-Console.WriteLine("Escribio: " + texto);
+//readline devuelve un string
+
+Console.WriteLine("Ingrese un numero:");
+string numero1 = Console.ReadLine();
+Console.WriteLine("Ingrese otro numero:");
+string numero2 = Console.ReadLine();
+
+int resultado = Convert.ToInt32(numero1) + Convert.ToInt32(numero2);
+
+// int resultado = Int32.Parse(numero1) + Int32.Parse(numero2);
+
+Console.WriteLine("La suma de los numeros es: " + resultado);
