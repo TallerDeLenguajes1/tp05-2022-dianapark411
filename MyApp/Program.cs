d8d9ea5ec6ec0d7e0ae480116f8fad1ac5823b8f
Diana Park <dianapark411@gmail.com>
2022-05-17 15:30:39 -0300

    cuarto commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Ingrese un numero:");
+Console.WriteLine("Ingrese texto:");
 string texto = Console.ReadLine();
 Console.WriteLine("Escribio: " + texto);
