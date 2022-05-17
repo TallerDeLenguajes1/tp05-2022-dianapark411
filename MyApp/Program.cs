124b5a3ea7773bcf562f7fb140f4e46a9f06003a
Diana Park <dianapark411@gmail.com>
2022-05-17 15:59:16 -0300

    quinto commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Ingrese texto:");
 string texto = Console.ReadLine();
@@ -11,7 +10,11 @@ Console.WriteLine("Ingrese otro numero:");
 string numero2 = Console.ReadLine();
 
 int resultado = Convert.ToInt32(numero1) + Convert.ToInt32(numero2);
-
 // int resultado = Int32.Parse(numero1) + Int32.Parse(numero2);
 
+string resultado2 = numero1 + numero2;
+
+
+Console.WriteLine("La suma de los numeros es: " + resultado2);
 Console.WriteLine("La suma de los numeros es: " + resultado);
+
