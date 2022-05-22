c5c0484b1504e21adbb2ab967f6d7a1bfb6bcddc
Diana Park <dianapark411@gmail.com>
2022-05-22 01:58:21 -0300

    ejercicio 4 avances

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -82,6 +82,74 @@ void CalculadoraV2 () {
 CalculadoraV1();
 CalculadoraV2();
 
+
+//EJERCICIO 4: CADENAS
+
+Console.WriteLine("Ingrese una cadena: ");
+string cadena1 = Console.ReadLine();
+
+// Obtener algunas letras que forman parte de una cadena -> nose a que se refiere porque mas abajo pide que extraiga una subcadena
+
+Console.WriteLine("Longitud de la cadena: " + cadena1.Length);
+Console.WriteLine("Subcadena: " + cadena1.Substring(1,5));
+
+cadena1 = cadena1.ToUpper();
+Console.WriteLine("Cadena en mayusculas: " + cadena1);
+cadena1 = cadena1.ToLower();
+Console.WriteLine("Cadena en minusculas: " + cadena1);
+
+Console.WriteLine("Cadena leida caracter por caracter:");
+foreach (char c in cadena1){
+    Console.WriteLine(c);
+}
+
+List<string> array = new List<string>();
+array = cadena1.Split(' ').ToList();    //guardo cada palabra de la cadena en un array
+	
+//string[] array = cadena.Split(delimitador);
+
+Console.WriteLine("Ingrese la palabra de la que quiere saber su ocurrencia: ");
+string palabra = Console.ReadLine();
+int cantOcurrencias = 0;
+
+foreach (var l in array){
+    if(l.CompareTo(palabra) == 0){
+        cantOcurrencias ++;
+    }
+}
+Console.WriteLine("Cantidad de veces que aparece " + palabra + " en " + cadena1 + ": " + cantOcurrencias);
+
+
+Console.WriteLine("Ingrese otra cadena: ");
+string cadena2 = Console.ReadLine();
+Console.WriteLine("Cadena 1 y 2 unidas: " + cadena1 + " " + cadena2);
+
+int valor = cadena1.CompareTo(cadena2);
+if(valor == 0){
+    Console.WriteLine("Las cadenas 1 y 2 son iguales");
+}else{
+    if(valor<0){
+        Console.WriteLine("La cadena 1 es lexicograficamente menor que la cadena 2");
+    }else{
+        Console.WriteLine("La cadena 1 es lexicograficamente mayor que la cadena 2");
+    }
+}
+
+Console.WriteLine("Ingrese una ecuacion (Simbolos: Suma-> +, Resta-> -, Multiplicacion-> x, Division-> / ): ");
+string ecuacion = Console.ReadLine();
+
+/*
+
+● Utilizando la calculadora creada anteriormente realizar las operaciones de dos números y mostrar por pantalla y mostrar en texto el resultado. Por ejemplo para la suma sería:
+“la suma de “ num1 “ y de” num2 “ es igual a: ” resultado.
+Donde num1, num2 y resultados son los sumandos y el resultado de la operación respectivamente.
+Nota: Busque el comportamiento del Método ToString();
+
+● Ingrese una cadena separada por caracteres que usted determine y muestre por pantalla los resultados (Revisar el comportamiento de split())
+
+● Siguiendo con el ejemplo de la calculadora (ejercicio 2) ingrese una ecuación simple como cadena de caracteres y que el sistema lo resuelva. Por ej. ingrese por pantalla “582+2” y que le devuelva la suma de 582 con 2
+*/
+
 /*
 Console.WriteLine("Ingrese texto:");
 string texto = Console.ReadLine();
